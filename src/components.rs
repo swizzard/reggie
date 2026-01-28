2dab9fda95cc4eda9f601978095a6c8e994b6340
Sam Raker <sam.raker@gmail.com>
2026-01-28 10:19:40 -0500

    start on group indices

--- src/components.rs
+++ src/components.rs
@@ -14,3 +14,70 @@ pub use flags::Flags;
 pub use groups::Group;
 pub use pattern::Pattern;
 pub use quantifiers::Quantifier;
+
+#[derive(Debug)]
+pub struct GroupIndices<'a> {
+    pat: &'a Pattern,
+    named: HashMap<String, &'a SubPattern>,
+    indexed: Vec<&'a SubPattern>,
+}
+
+impl<'a> GroupIndices<'a> {
+    fn new(pat: &'a Pattern) -> Self {
+        let mut indexed = Vec::with_capacity(pat.sub_patterns.len());
+        let mut named = HashMap::new();
+        GroupIndices::collect_component_groups(&mut indexed, &mut named, &pat.sub_patterns);
+        Self {
+            pat,
+            named,
+            indexed,
+        }
+    }
+    fn collect_component_groups(
+        indexed: &mut Vec<&'a SubPattern>,
+        named: &mut HashMap<String, &'a SubPattern>,
+        cs: &'a Vec<SubPattern>,
+    ) {
+        for c in cs.iter() {
+            match c {
+                SubPattern::Group(Group::Group {
+                    ext,
+                    name,
+                    components,
+                    ..
+                }) => {
+                    if let Some(GroupExt::NonCapturing) = ext {
+                        continue;
+                    } else {
+                        if let Some(name) = name {
+                            named.insert(name.to_string(), c);
+                        };
+                        indexed.push(c);
+                        GroupIndices::collect_component_groups(indexed, named, components);
+                    }
+                }
+                _ => continue,
+            }
+        }
+    }
+}
+
+#[derive(Debug, Eq, Hash, PartialEq)]
+pub enum GroupIndex {
+    Numbered(usize),
+    Named(String),
+}
+
+impl<T> From<T> for GroupIndex
+where
+    String: From<T>,
+{
+    fn from(value: T) -> Self {
+        let value = String::from(value);
+        if let Ok(num) = value.parse::<usize>() {
+            Self::Numbered(num)
+        } else {
+            Self::Named(value)
+        }
+    }
+}
